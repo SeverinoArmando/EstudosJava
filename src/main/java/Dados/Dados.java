cf1c4aad6056a60ac6c59aeec03ec4c924773a42
SeverinoArmando <severinoarmando3@gmail.com>
2021-07-10 14:06:15 -0300

    Novos elementos

---
+++ src/main/java/Dados/Dados.java
@@ -0,0 +1,31 @@
+package Dados;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Dados {
+    private String nome;
+    private LocalDate dataDeNascimento;
+
+    public Dados (String nome, int dia, int mes, int ano) {
+        this.nome = nome;
+        this.dataDeNascimento = LocalDate.of(ano, mes, dia);
+    }
+
+    public int calcularIdade() {
+        return Period.between(dataDeNascimento, LocalDate.now()).getYears();
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public LocalDate getDataDeNascimento(){
+        return dataDeNascimento;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
+
